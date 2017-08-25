2972b4f563c2cdb921ab1ab7ec69c818e46affc1
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-08-25 16:36:16 -0300

    Simple socket server created to allow multiple chat windows

--- src/Components/ChatApp/ChatApp.js
+++ src/Components/ChatApp/ChatApp.js
@@ -1,7 +1,5 @@
-
-//var config = require('../config/index.js');
 import React from 'react';
-//import io from 'socket.io-client';
+import io from 'socket.io-client';
 import Messages from '../Messages/Messages';
 import ChatInput from '../ChatInput/ChatInput';
 import './ChatApp.css'
@@ -10,12 +8,12 @@ class ChatApp extends React.Component {
   constructor(props){
     super(props);
     this.state = { messages: [] };
-   // this.socket = io(config.exports.api).connect()
+    this.socket = io('http://localhost:3000/').connect()
     this.sendHandler = this.sendHandler.bind(this);
     this.addMessage = this.addMessage.bind(this);
-    ///this.socket.on('server:message', message => {
-    ///  this.addMessage(message);
-   // });
+    this.socket.on('server:message', message => {
+      this.addMessage(message);
+    });
   }
 
   sendHandler(message){
@@ -24,7 +22,7 @@ class ChatApp extends React.Component {
       message
     };
 
-   // this.socket.emit('client:message', messageObject);
+    this.socket.emit('client:message', messageObject);
     messageObject.fromMe = true;
     this.addMessage(messageObject);
   }

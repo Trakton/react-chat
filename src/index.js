534a6de561b6b70c796ceb442775706bb7ccc9fe
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-08-28 10:54:04 -0300

    Adding favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Components/App/App';
 import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
